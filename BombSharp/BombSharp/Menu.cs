8997b9a2170a7acd3b7b7aa20f1c3f1469904454
gabriel-bizioo <gabriel.bizio@ufpr.br>
2022-05-02 11:13:08 -0300

    working on animations

--- BombSharp/BombSharp/Menu.cs
+++ BombSharp/BombSharp/Menu.cs
@@ -16,30 +16,28 @@ namespace BombSharp
         System.Windows.Forms.Timer tm = new System.Windows.Forms.Timer();
         
         SoundPlayer player = null;
-
         Bitmap bmp = null;
         Graphics g = null;
+        Image img = new Bitmap(Properties.sprites.walk_animations);
 
-        Image img = new Bitmap(@"C:\repos\BombSharp\Sprites\spritesheet.png");
-
-        
-        
+                
         public Menu()
         {
             InitializeComponent();
-            player = new SoundPlayer(@"C:\repos\BombSharp\Audio\Hey Ya but it's super compressed - YouTube.MP4.wav");
+            player = new SoundPlayer(Properties.audio.heya);
         }
 
+        //incremento do y = 25
+        //incremento do x = 17
+        int x = 3;
+        int y = 2;
 
-        int x = 59;
-        int y = 47;
-        bool front = true;
 
         private void Menu_Load(object sender, EventArgs e)
         {
             bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
             g = Graphics.FromImage(bmp);
-            tm.Interval = 300;
+            tm.Interval = 200;
             tm.Tick += new EventHandler(tm_Tick);
             tm.Start();
             player.Play();
@@ -47,19 +45,19 @@ namespace BombSharp
 
         private void tm_Tick(Object sender, EventArgs e)
         {
-            g.Clear(Color.FromArgb(255, 233, 127));
+            g.Clear(Color.White);
 
-            if(x <= 127)
+            if(x <= 88)
             {
-                g.DrawImage(img, new Rectangle(0, 0, (pictureBox1.Width+200), pictureBox1.Height), new Rectangle(x, y, 15, 23), GraphicsUnit.Pixel);
+                g.DrawImage(img, new Rectangle(20, 20, (pictureBox1.Width/12), (pictureBox1.Height/12)), new Rectangle(x, y, 14, 24), GraphicsUnit.Pixel);
 
                 x += 17;
             }
             else
             {
-                x = 59;
+                x = 3;
                 
-                g.DrawImage(img, new Rectangle(0, 0, (pictureBox1.Width+200), pictureBox1.Height), new Rectangle(x, y, 15, 23), GraphicsUnit.Pixel);
+                g.DrawImage(img, new Rectangle(20, 20, (pictureBox1.Width/12), (pictureBox1.Height/12)), new Rectangle(x, y, 14, 24), GraphicsUnit.Pixel);
 
                 x += 17;
             }
